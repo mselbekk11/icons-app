766d25e54567a5867fc8dc6657469aadfbbd9144
mselbekk11 <mselbekk11@gmail.com>
2025-01-17 22:01:26 -0800

    updates

--- src/components/icon-square.tsx
+++ src/components/icon-square.tsx
@@ -1,4 +1,5 @@
 import type { IconData } from "~/data/data";
+import { Button } from "./ui/button";
 
 interface IconSquareProps {
   index: number;
@@ -6,14 +7,30 @@ interface IconSquareProps {
 }
 
 export default function IconSquare({ index, icon }: IconSquareProps) {
+  const copyToClipboard = () => {
+    void navigator.clipboard
+      .writeText(icon.svg)
+      .then(() => {
+        alert("Copied to clipboard!");
+      })
+      .catch((err) => {
+        console.error("Failed to copy:", err);
+      });
+  };
+
   return (
     <div
       key={index}
       className="bg-custom-grey dark:bg-custom-dark w-25 group relative flex h-24 items-center justify-center rounded-lg border transition-shadow hover:shadow-lg"
     >
       <icon.icon className="h-8 w-8" />
-      <div className="absolute -bottom-[22px] z-10 hidden truncate rounded-md bg-[#f56565] px-2 py-1 text-xs text-white transition-shadow hover:shadow-lg group-hover:block dark:text-white">
-        {icon.title}
+      <div className="absolute inset-0 flex hidden flex-col items-center justify-center gap-2 rounded-lg backdrop-blur-md group-hover:flex">
+        <button
+          onClick={copyToClipboard}
+          className="rounded-md bg-[#f56565] px-2 py-1 text-xs text-[#fff]"
+        >
+          Copy SVG
+        </button>
       </div>
     </div>
   );
--- src/data/data.ts
+++ src/data/data.ts
@@ -8,6 +8,7 @@ import Five from "~/components/icons/5";
 export interface IconData {
   title: string;
   icon: React.FC<React.SVGProps<SVGSVGElement>>;
+  svg: string;
   categories: string[];
   tags: string[];
 }
@@ -17,30 +18,35 @@ export const Data: IconData[] = [
   { 
     title: "Alarm Clock", 
     icon: One,
+    svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z" /><path d="M10 2c1 .5 2 2 2 5" /></svg>`,
     categories: ["one", "two", "three"],
     tags: ["morning"]
   },
   { 
     title: "Apple", 
     icon: Two,
+    svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z" /><path d="M10 2c1 .5 2 2 2 5" /></svg>`,
     categories: ["one", "four", "seven"],
     tags: ["fruit", "food"] // Example categories
   },
   { 
     title: "Anchor", 
     icon: Three,
+    svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z" /><path d="M10 2c1 .5 2 2 2 5" /></svg>`,
     categories: ["seven", "one", "eight"],
     tags: ["ship"]
   },
   { 
     title: "Atom", 
     icon: Four,
+    svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z" /><path d="M10 2c1 .5 2 2 2 5" /></svg>`,
     categories: ["three", "four", "five","seven", "one", "eight"],
     tags: ["atomic","nuclear","physics","particle","element","molecule","electricity","energy","chemistry"]
   },
   { 
     title: "Badge Dollar Sign", 
     icon: Five,
+    svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z" /><path d="M10 2c1 .5 2 2 2 5" /></svg>`,
     categories: ["two", "three", "seven", "five"],
     tags: ["atomic","nuclear","physics","particle","element","molecule","electricity","energy","chemistry"]
   },
