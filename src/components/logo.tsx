d96f2f9ac0a1b79ad734819cd8c5cd130ce2d9b6
mselbekk11 <mselbekk11@gmail.com>
2025-01-18 14:29:57 -0800

    updates

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,11 @@
 import { BadgeDollarSign } from "lucide-react";
 import Lottie from "lottie-react";
-import fire from "~/assets/fire.json";
+import fireOne from "~/assets/fire-outline.json";
 export default function Logo() {
   return (
     <div className="flex h-16 items-center justify-center gap-2 border-b">
       {/* <BadgeDollarSign /> */}
-      <Lottie animationData={fire} className="h-8 w-8" />
+      <Lottie animationData={fireOne} className="h-8 w-8" />
       <span className="text-lg">Fire Icons</span>
       {/* <span>MZY Icons</span> */}
     </div>
