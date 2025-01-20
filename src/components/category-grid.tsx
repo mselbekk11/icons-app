eaa52d7df17ee06f2d9cc01c7e41c68d2ea2ea57
mselbekk11 <mselbekk11@gmail.com>
2025-01-19 17:30:13 -0800

    updates

--- src/components/category-grid.tsx
+++ src/components/category-grid.tsx
@@ -44,15 +44,6 @@ export default function CategoryGrid() {
             </h2>
             <div className="grid w-full grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-2">
               {categoryIcons.map((icon, idx) => (
-                // <div
-                //   key={`${category}-${idx}`}
-                //   className="w-25 group relative flex h-24 items-center justify-center rounded-lg border transition-shadow hover:shadow-lg dark:bg-[#161617]"
-                // >
-                //   <item.icon className="h-8 w-8" />
-                //   <div className="absolute -bottom-[22px] z-10 hidden truncate rounded-md bg-[#f56565] px-2 py-1 text-xs text-white transition-shadow hover:shadow-lg group-hover:block">
-                //     {item.title}
-                //   </div>
-                // </div>
                 <IconSquare key={idx} index={idx} icon={icon} />
               ))}
             </div>
