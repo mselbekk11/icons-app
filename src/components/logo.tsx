a640e663a698f039eb1c6bf4d9bcd8de6506d3b0
mselbekk11 <mselbekk11@gmail.com>
2025-01-21 17:51:58 -0800

    updates

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import LogoTwo from "./logo-two";
+
 export default function LogoNav() {
   return (
     <div className="flex h-16 items-center justify-center gap-2 border-b">
       {/* <Lottie animationData={fireOne} className="h-8 w-8" /> */}
+      <LogoTwo />
       <span className="text-lg">Fire Icons</span>
     </div>
   );
